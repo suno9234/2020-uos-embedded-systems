c00e5abdba7806b17aa96f24657290920077c0ec
suno9234 <74644225+suno9234@users.noreply.github.com>
2020-11-23 16:28:01 +0900

    Update rc_car_interface.py

--- src/raspberry/TF_core/rc_car_interface.py
+++ src/raspberry/TF_core/rc_car_interface.py
@@ -5,6 +5,7 @@ __author__ = 'will'
 
 import numpy as np
 import cv2
+import serial
 
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -17,15 +18,21 @@ class RC_Car_Interface():
         self.camera = PiCamera()
         self.camera.resolution = (320,320)         # set camera resolution to (320, 320)
         self.camera.color_effects = (128,128)      # set camera to black and white
+        self.ser= serial.Serial('/dev/ttyUSB0',9600)
 
     def finish_iteration(self):
         print('finish iteration')
 
     def set_right_speed(self, speed):
         print('set right speed to ', speed)
+        cmd = ("R%d\n" % speed).encode('ascii')
+        ser.write(cmd)
     
     def set_left_speed(self, speed):
         print('set left speed to ', speed)
+        cmd = ("L%d\n" % speed).encode('ascii')
+        print("My cmd is %s" % cmd)
+        ser.write(cmd)
         
     def get_image_from_camera(self):
         img = np.empty((320, 320, 3), dtype=np.uint8)
