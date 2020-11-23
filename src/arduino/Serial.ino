6d067629313ad4446e1025bdad300e281e8ad70d
suno9234 <74644225+suno9234@users.noreply.github.com>
2020-11-23 16:29:29 +0900

    Update Serial.ino

--- src/arduino/Serial.ino
+++ src/arduino/Serial.ino
@@ -1,43 +1,50 @@
 String Speed;
-char LorR;
-int i, s;
+char  LorR;
+int  i, s;
 
+#define motorR_1 4
+#define motorR_2 5
+#define motorL_1 6
+#define motorL_2 7
+#define motorL_SpeedControler 3
+#define motorR_SpeedControler 9
 byte DataToRead[6];
 
-void setup()
-{
-	// put your setup code here, to run once:
-	Serial.begin(9600);
+void setup() {
+  // put your setup code here, to run once:
+  //Serial.begin(9600);
+  pinMode(motorL_1,OUTPUT);
+  pinMode(motorL_2,OUTPUT);
+  pinMode(motorR_1,OUTPUT);
+  pinMode(motorR_2,OUTPUT);
+  pinMode(motorL_SpeedControler,OUTPUT);
+  pinMode(motorR_SpeedControler,OUTPUT);
+  
 }
 
-void loop()
-{
-	DataToRead[5] = '\n';
-	Serial.readBytesUntil(char(13), DataToRead, 5);
-
-	/* For Debugging, send string to RPi */
-	for (i = 0; i < 6; i++)
-	{
-		Serial.write(DataToRead[i]);
-		if (DataToRead[i] == '\n')
-			break;
-	}
-	/* End of Debugging */
-
-	LorR = DataToRead[0];
-	Speed = "";
-	for (i = 1; (DataToRead[i] != '\n') && (i < 6); i++)
-	{
-		Speed += DataToRead[i];
-	}
-	s = Speed.toInt();
-	if (LorR == 'L')
-	{
-		// Turn left wheel with speed s
-	}
-	else if (LorR == 'R')
-	{
-		// Turn right wheel with speed s
-	}
-	delay(2000);
+void loop() {
+  // 1-HIGH 2-LOW -> 앞으로가기
+  analogWrite(motorL_SpeedControler,255);
+  analogWrite(motorR_SpeedControler,255);
+  digitalWrite(motorL_1,HIGH);
+  digitalWrite(motorL_2,LOW);
+  digitalWrite(motorR_1,HIGH);
+  digitalWrite(motorR_2,LOW);
+  
+  DataToRead[5] = '\n';
+  Serial.readBytesUntil(char(13), DataToRead, 5);
+  
+  LorR = DataToRead[0];
+  Speed = "";
+  for (i = 1; (DataToRead[i] != '\n') && (i < 6); i++) {
+    Speed += DataToRead[i];
+  }
+  s = Speed.toInt();
+  if (LorR == 'L') {
+    analogWrite(motorL_SpeedControler,s);
+  }
+  else if (LorR == 'R') {
+    // Turn right wheel with speed s
+    analogWrite(motorR_SpeedControler,s);  
+  } 
 }
